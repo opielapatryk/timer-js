3cce69595a194f9e63d10ee484c4bc7ccd00a23f
opielapatryk <xpatrykopiela@gmail.com>
2022-07-07 13:02:52 +0200

    master

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const ending = document.querySelector(".ending")
 const div = document.querySelector("div")
 const hours = function (date) {
     let godzina = date.getHours()
+    let minuta = date.getMinutes()
     let amorpm = godzina < 12 ? 'AM' : "PM"
     godzina = godzina / 12 == 1 ? godzina = 12 : godzina = godzina % 12 
     godzina = godzina < 10 ? '0' + godzina : godzina
-    let minuta = date.getMinutes()
     minuta = minuta < 10 ? '0' + minuta : minuta
-    return godzina + ':' + minuta + amorpm
+    return div.innerHTML = godzina + ':' + minuta + amorpm 
 }
-
-div.innerHTML = hours(new Date())
+setInterval(()=>hours(new Date()), 1000)
